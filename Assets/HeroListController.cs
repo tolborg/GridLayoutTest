8898674090042c9c548b3f922e7023fce24fa75f
Anders Tolborg <anderstolborg@sybogames.com>
2016-10-13 16:07:16 +0200

    Blah

---
+++ Assets/HeroListController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HeroListController : MonoBehaviour 
+{
+	public GameObject _content;
+	public GameObject _heroListItemPrefab;
+	public int _heroCount;
+
+	private int[] rarities = { 1,1,2,3,2,1,2,3,2,3,2,2,3,3,1,1,2,2,3,3,3,2,1,1,2,3 };
+
+
+	void Start() 
+	{
+		foreach (int rarity in rarities)
+		{
+			GameObject heroListItem = Instantiate(_heroListItemPrefab, _content.transform) as GameObject;
+			heroListItem.GetComponent<HeroListItemController>()._rarity = rarity;
+			heroListItem.GetComponent<HeroListItemController>()._rarityText.GetComponent<Text>().text = rarity.ToString();
+		}
+	}
+
+
+
+	public void FilterHeroes(int rarity)
+	{
+		foreach (Transform heroListItem in _content.transform)
+		{
+			heroListItem.GetComponent<HeroListItemController>().SomeFunction(rarity);
+
+
+//			if (rarity == 0 || heroListItem.GetComponent<HeroListItemController>()._rarity == rarity)
+//			{
+//				heroListItem.GetComponent<HeroListItemController>().Activate();
+//			}
+//			else
+//			{
+//				heroListItem.GetComponent<HeroListItemController>().Deactivate();
+//			}
+		}
+
+	}
+		
+
+}
---
+++ Assets/HeroListItemController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HeroListItemController : MonoBehaviour 
+{
+	public int _rarity;
+	public GameObject _rarityText;
+
+
+	public void SomeFunction(int rarity)
+	{
+
+
+		StartCoroutine(Deactivate(() => {
+
+			if (rarity == 0 || rarity == _rarity)
+			{
+				StartCoroutine(Activate());
+			}
+				
+		}));
+
+	}
+
+
+
+
+
+
+
+
+	IEnumerator Deactivate(System.Action callback = null)
+	{
+		float i = 0;
+		float rate = 1 / 2f;
+
+		while (i < 1)
+		{
+			i += Time.deltaTime * rate;
+			gameObject.GetComponent<CanvasGroup>().alpha = 1 - i;
+			yield return null;
+		}
+
+		gameObject.GetComponent<LayoutElement>().ignoreLayout = true;
+			
+		if (callback != null) { callback(); }
+	}
+
+
+	IEnumerator Activate(System.Action callback = null)
+	{
+		gameObject.GetComponent<LayoutElement>().ignoreLayout = false;
+
+		float i = 0;
+		float rate = 1 / 2f;
+
+		while (i < 1)
+		{
+			i += Time.deltaTime * rate;
+			gameObject.GetComponent<CanvasGroup>().alpha = i;
+			yield return null;
+		}
+			
+		if (callback != null) { callback(); }
+	}
+
+
+
+
+
+
+}
